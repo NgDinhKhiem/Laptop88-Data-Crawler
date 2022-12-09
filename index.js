1f1016aa1dae5f824e06c6995f957c902e143e72
NgDinhKhiem <87563740+NgDinhKhiem@users.noreply.github.com>
2022-12-09 21:35:46 +0700

    More Data is Crawl

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const start = window.performance.now();
 
 let arr = [];
 const url = "https://laptop88.vn/laptop-moi.html?page=";
-const urn = "https://laptop88.vn/";
+const urn = "https://laptop88.vn";
 
 async function getList(page) {
     const rawData = await c(url + page).send();
@@ -26,12 +26,18 @@ async function getList(page) {
     let cutData = data.substring(begin, end);
     const u = '<div class="product-item">';
     const a = '<div class="tooltip-content-group">';
+    var count = 0;
+    console.log(' ')
     while (cutData.search(u) != -1) {
         let st = cutData.search(u);
         let ed = cutData.search(a);
         let shortenData = cutData.substring(st, ed + a.length);
         cutData = cutData.substring(ed + a.length, cutData.length);
         if (shortenData.length > 300) {
+
+            process.stdout.moveCursor(0, -1);
+            process.stdout.clearLine(1);
+            console.log(++count);
             await extractData(shortenData)
         }
     }
@@ -39,6 +45,8 @@ async function getList(page) {
 }
 
 async function extractData(rawData) {
+    fs.writeFileSync('test.html', rawData);
+    //extractURI(rawData);
     var data = {
         "NAME": "",
         "CPU": "",
@@ -46,8 +54,11 @@ async function extractData(rawData) {
         "DISK": "",
         "GPU": "",
         "SIZE": "",
+        "BATTERY": "",
+        "WEIGHT": "",
         "PRICE": ""
     }
+    let extra = await getWandB(extractURI(rawData));
     try {
         let s1 = rawData.search('<!---<div class="icon_2022 price_shock"></div>--->');
         let s2 = rawData.substring(0, s1);
@@ -68,12 +79,15 @@ async function extractData(rawData) {
         data.DISK = jsonTable[2]['1'];
         data.GPU = jsonTable[3]['1'];
         data.SIZE = jsonTable[4]['1'];
+        data.BATTERY = extra[1];
+        data.WEIGHT = extra[0];
         let ct1 = '<span class="item-price">';
         let it1 = rawData.search(ct1);
         let st1 = rawData.substring(it1 + ct1.length, rawData.length);
         let it2 = st1.search('</span>');
         let price = st1.substring(0, it2);
         data.PRICE = price;
+        //console.log(data);
         arr.push(data);
     } catch (err) {
         console.log(err);
@@ -82,13 +96,57 @@ async function extractData(rawData) {
 
 async function run() {
     for (var i = 1; i <= 14; i++) {
-        await getList(i);
         console.clear();
         console.log(i + '/14');
+        await getList(i);
     }
     fs.writeFileSync('data.xlsx', json2xls(arr), 'binary')
     const end = window.performance.now();
     console.log((end - start) / 1000 + "ms");
 }
 
+console.log('STARTING')
 run();
+
+//getWandB('https://laptop88.vn/moi-100-full-box-laptop-asus-tuf-f15-2022-fa507rc-hn051w-amd-ryzen-7-6800h-rtx-3050-144hz.html');
+
+function extractURI(html) {
+    const st = '<a href';
+    let cutData = html.substring(html.search(st) + st.length + 2, html.length);
+    cutData = cutData.substring(0, cutData.search('>') - 1);
+    return urn + cutData;
+}
+
+async function getWandB(uri) {
+    const html = await c(uri).send();
+    const rawData = await html.text();
+    //fs.writeFileSync(`${counter++}.html`, rawData)
+    let a = '<table style="height: 1316px;" width="524" data-tablestyle="MsoNormalTable" data-tablelook="0">';
+    const b = '</table>';
+    if (rawData.search(a) == '-1')
+        a = '<span>Thông số kỹ thuật</span>'
+        //return ['NA', 'NA']
+    let cutData = rawData.substring(rawData.search(a), rawData.length);
+    cutData = cutData.substring(0, cutData.search(b));
+    cutData = cutData.toLocaleLowerCase();
+    const tk = 'kg';
+    var kg = 'NA';
+    let i_kg = cutData.search(tk);
+    if (i_kg != '-1') {
+        let data_kg = cutData.substring(i_kg - 20, i_kg - 1);
+        data_kg = data_kg.substring(data_kg.lastIndexOf('>') + 1, data_kg.length);
+        kg = data_kg;
+    }
+    let pin = 'NA';
+    let i_pin = cutData.search('wh');
+    if (i_pin != '-1') {
+        let data_pin = cutData.substring(i_pin - 10, i_pin);
+        let lastClose = data_pin.lastIndexOf('>');
+        data_pin = cutData.substring(i_pin - 8 + lastClose, i_pin + 50);
+        data_pin = data_pin.substring(0, data_pin.search('<'));
+        pin = data_pin;
+    }
+    //console.log(uri)
+
+    return [kg, pin];
+}
